06c51c2bcfb225bb450996260dca755be6921da6
luozf <24006765@qq.com>
2019-08-05 17:46:04 +0800

    args

--- php/startup.sh
+++ php/startup.sh
@@ -3,7 +3,7 @@
 time=$(date "+%Y-%m-%d %H:%M:%S")
 echo "startup ${time}" >> /root/startup.log 2>&1  &
 
-# php-fpm
-/usr/sbin/service php-fpm start
+php-fpm -D
+# /usr/sbin/service php-fpm start
 
 tail -f /dev/null
